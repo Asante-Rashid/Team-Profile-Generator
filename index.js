77816aa562478a732539c7876f92d22d159c0eaf
Asante <asanterashid.biz@gmail.com>
2021-05-09 19:27:40 -0400

    added switch cases for engineers and interns

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ function init() {
             default: 'NA',
 
         },
+        {
+            type: "input",
+            name: "office-M",
+            message: "What is your office number?",
+            default: 'NA',
+
+        },
               {
             type: "checkbox",
             message: "End of profile. What would yoiu like to do?",
@@ -39,14 +46,116 @@ function init() {
                 "end team builder",
             ],
 
+        }
+    ]).then(finalAction => {
+        switch (finalAction.choices){
+            case "add engineer": addEnginer();
+            case "add intern": addIntern();
+            case "end team builder": completeTeam();
+        }
+    })
+};
+
+function addEnginer() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Name-M",
+            message: "What is your name?",
+            default: 'Asante',
+
         },
         {
-            type: 'input',
-            name: 'url',
-            message: 'Enter the URL',
-            when: (answers) => answers.Choice === "add engineer"
-         },
-    ])
+            type: "input",
+            name: "id-M",
+            message: "What is your employee ID number",
+            default: '00000',
+
+        },
+        {
+            type: "input",
+            name: "email-M",
+            message: "What is your email?",
+            default: 'NA',
+
+        },
+        {
+            type: "input",
+            name: "github-M",
+            message: "What is your GitHub?",
+            default: 'NA',
+
+        },
+              {
+            type: "checkbox",
+            message: "End of profile. What would yoiu like to do?",
+            name: "Choice",
+            choices: [
+                "add engineer",
+                "add intern",
+                "end team builder",
+            ],
+
+        }
+    ]).then(finalAction => {
+        switch (finalAction.choices){
+            case "add engineer": addEnginer();
+            case "add intern": addIntern();
+            case "end team builder": completeTeam();
+        }
+    })
 };
+
+function addIntern() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Name-M",
+            message: "What is your name?",
+            default: 'Asante',
+
+        },
+        {
+            type: "input",
+            name: "id-M",
+            message: "What is your employee ID number",
+            default: '00000',
+
+        },
+        {
+            type: "input",
+            name: "email-M",
+            message: "What is your email?",
+            default: 'NA',
+
+        },
+        {
+            type: "input",
+            name: "school-M",
+            message: "What is your school?",
+            default: 'NA',
+
+        },
+              {
+            type: "checkbox",
+            message: "End of profile. What would yoiu like to do?",
+            name: "Choice",
+            choices: [
+                "add engineer",
+                "add intern",
+                "end team builder",
+            ],
+
+        }
+    ]).then(finalAction => {
+        switch (finalAction.choices){
+            case "add engineer": addEnginer();
+            case "add intern": addIntern();
+            case "end team builder": completeTeam();
+        }
+    })
+};
+
+function completeTeam();
 
 init();
